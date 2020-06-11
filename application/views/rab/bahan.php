8d0d53393247f92dcbc628654481cd9b47c336d0
rizaldyputra <rizaldyputra@users.github.com>
2020-06-11 20:50:17 +0700

    views rab

---
+++ application/views/rab/bahan.php
@@ -0,0 +1,30 @@
+
+  <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Transaksi Harga Bangunan</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <tr bgcolor="#f7f7f7" align="center">
+                      <td>No</td>
+                      <td>Id Bahan</td>
+                      <td>Nama Bahan</td>
+                      <td>Jumlah</td>
+                      <td>Harga Satuan</td>
+                      <td>Harga Total</td>
+                      <td>Id Projek</td>
+                    </tr>
+      </table>
+     </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      </div>
+          <!-- Page Heading -->
---
+++ application/views/rab/jmlbiaya.php
@@ -0,0 +1,32 @@
+
+  <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Jumlah Biaya Pekerja</h6>
+            <div class="d-sm float-right align-items-center justify-content-between mb-2">
+             <a href="tambahUser" class="btn btn-primary btn-icon-split">
+          <span class="text">+ Tambah Data</span>
+    </a></div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <tr bgcolor="#f7f7f7" align="center">
+                      <td>No</td>
+                      <td>Id Jumlah Biaya</td>
+                      <td>Id Projek</td>
+                      <td>Uraian</td>
+                      <td>Volume</td>
+                      <td>Luas</td>
+                      <td>Harga Satuan</td>
+                      <td>Harga Total</td>
+                      <td>Tindakan</td>
+                    </tr>
+                    </table>
+                    <tbody>
+                      
+                    </tbody> 
+                    </table>
+    </div>
+  </div>
+</div>
+</div>
+        
